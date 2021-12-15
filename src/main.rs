56070322173f4c46ffd1326c3e6c641e685a980a
Rodrigo de França <howdrigo.fr@gmail.com>
2021-12-15 15:02:26 -0300

    feat: implemented operate and output functions

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use std::env::{args, Args};
+
+fn main() {
+    let mut args: Args = args();
+
+    let first = args.nth(1).unwrap();
+    let operator = args.nth(0).unwrap().chars().next().unwrap();
+    let second = args.nth(0).unwrap();
+
+    let first_number = first.parse::<f32>().unwrap();
+    let second_number = second.parse::<f32>().unwrap();
+
+    let result = operate(operator, first_number, second_number);
+
+    println!("{:?}", output(first_number, second_number, operator, result));
+}
+
+fn operate(operator: char, first: f32, second: f32) -> f32 {
+    match operator {
+        '+' => first + second,
+        '-' => first - second,
+        '*' | 'x' | 'X' => first * second,
+        '/' => first / second,
+        _ => panic!("Unknown operator"),
+    }
+}
+
+fn output(first_number: f32, second_number: f32, operator: char, result: f32) -> String {
+    format!("{} {} {} = {}", first_number, operator, second_number, result)
+}
